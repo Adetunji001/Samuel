b882d66cd6178986f7e9527d7cbaaf81330e9a7e
Adetunji001 <samueladetunji014@gmail.com>
2023-10-17 20:56:36 +0100

    samuel

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $name = 'Ishiaku paul';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SWE 305</title>
+</head>
+<body>
+   <h1 style="color: yellow; background-color: black; padding: 50gpx">Welcome! <?= $name ?></h1>  
+</body>
+</html>
